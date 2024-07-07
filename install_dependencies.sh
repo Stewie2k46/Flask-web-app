bcfc2d37feb19c155bd7777f09d184c4cab6f53d
Stewie2k46 <stewie2k956@gmail.com>
2024-07-07 19:12:28 +0530

    Update install_dependencies.sh

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -4,11 +4,16 @@
 sudo yum update -y
 sudo yum install -y python3 python3-pip
 
+# Navigate to the deployment directory
+cd /opt/codedeploy-agent/deployment-root/.../deployment-archive/
+
 # Install virtualenv
 pip3 install virtualenv
 
-# Navigate to the application directory and set up the environment
-cd /home/ec2-user/flask-app
+# Navigate to the application directory (replace with actual directory name)
+cd /opt/codedeploy-agent/deployment-root/.../deployment-archive/Flask-web-app/
+
+# Set up the virtual environment
 virtualenv venv
 source venv/bin/activate
 
