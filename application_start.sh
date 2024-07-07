e4d19c1de47796786a2505e93b94f239d5d26928
Stewie2k46 <stewie2k956@gmail.com>
2024-07-07 19:20:42 +0530

    Update and rename start_server.sh to application_start.sh

---
+++ application_start.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd /var/www/html/Flask-web-app
+pip install -r requirements.txt
+python app.py &
--- start_server.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Start the Flask application
-cd /home/ec2-user/flask-app
-source venv/bin/activate
-nohup python3 app.py > app.log 2>&1 &
