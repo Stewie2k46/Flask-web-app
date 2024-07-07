81ab49aec106e38f4f55d93000f64ad7c709d25e
Stewie2k46 <stewie2k956@gmail.com>
2024-07-07 19:10:55 +0530

    Update install_dependencies.sh

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
-# Install Python and Flask
+# Install Python and pip
 sudo yum update -y
 sudo yum install -y python3 python3-pip
+
+# Install virtualenv
 pip3 install virtualenv
 
 # Navigate to the application directory and set up the environment
 cd /home/ec2-user/flask-app
 virtualenv venv
 source venv/bin/activate
+
+# Install dependencies from requirements.txt
 pip install -r requirements.txt
